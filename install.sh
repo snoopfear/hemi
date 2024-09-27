9b7ceaebe84714a322429b43a56c06eb18b33e8d
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-09-27 22:28:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,75 +1,145 @@
 #!/bin/bash
 
-echo "Welcome to the t3rn Executor Setup by snoopfear!"
+curl -s https://raw.githubusercontent.com/zunxbt/logo/main/logo.sh | bash
 
-# Установка ccze для цветного форматирования логов
-echo "Installing ccze for colored log formatting..."
-sudo apt update && sudo apt upgrade -y
-sudo apt -qy install ccze
+sleep 3
 
-# Проверка на существование переменной POPM_BTC_PRIVKEY
-if [ -z "${POPM_BTC_PRIVKEY}" ]; then
-    # Запрос ввода приватного ключа
-    read -p "Enter your BTC Private Key: " POPM_BTC_PRIVKEY
-    # Экспорт переменной
-    export POPM_BTC_PRIVKEY
-    
-    # Сохранение переменной в .bashrc для постоянного использования
-    echo "export POPM_BTC_PRIVKEY=\"$POPM_BTC_PRIVKEY\"" >> ~/.bashrc
-else
-    echo "Using existing POPM_BTC_PRIVKEY: $POPM_BTC_PRIVKEY"
+ARCH=$(uname -m)
+
+show() {
+    echo -e "\033[1;35m$1\033[0m"
+}
+
+if ! command -v jq &> /dev/null; then
+    show "jq not found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y jq > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        show "Failed to install jq. Please check your package manager."
+        exit 1
+    fi
 fi
 
-cd $HOME
 
-# Указание ссылки для загрузки и имени файла
-MINER_URL="https://github.com/hemilabs/heminetwork/releases/download/v0.4.3/heminetwork_v0.4.3_linux_amd64.tar.gz"
-MINER_FILE="heminetwork_v0.4.3_linux_amd64.tar.gz"
+check_latest_version() {
+    for i in {1..3}; do
+        LATEST_VERSION=$(curl -s https://api.github.com/repos/hemilabs/heminetwork/releases/latest | jq -r '.tag_name')
+        if [ -n "$LATEST_VERSION" ]; then
+            show "Latest version available: $LATEST_VERSION"
+            return 0
+        fi
+        show "Attempt $i: Failed to fetch the latest version. Retrying..."
+        sleep 2
+    done
 
-echo "Downloading the Executor binary from $MINER_URL..."
-curl -L -o $MINER_FILE $MINER_URL
-
-# Проверка успешности загрузки
-if [ $? -ne 0 ]; then
-    echo "Failed to download the Executor binary. Please check your internet connection and try again."
+    show "Failed to fetch the latest version after 3 attempts. Please check your internet connection or GitHub API limits."
     exit 1
+}
+
+check_latest_version
+
+
+download_required=true
+
+if [ "$ARCH" == "x86_64" ]; then
+    if [ -d "heminetwork_${LATEST_VERSION}_linux_amd64" ]; then
+        show "Latest version for x86_64 is already downloaded. Skipping download."
+        cd "heminetwork_${LATEST_VERSION}_linux_amd64" || { show "Failed to change directory."; exit 1; }
+        download_required=false  # Set flag to false
+    fi
+elif [ "$ARCH" == "arm64" ]; then
+    if [ -d "heminetwork_${LATEST_VERSION}_linux_arm64" ]; then
+        show "Latest version for arm64 is already downloaded. Skipping download."
+        cd "heminetwork_${LATEST_VERSION}_linux_arm64" || { show "Failed to change directory."; exit 1; }
+        download_required=false  # Set flag to false
+    fi
 fi
 
-echo "Extracting the binary..."
-tar -xzvf $MINER_FILE
+if [ "$download_required" = true ]; then
+    if [ "$ARCH" == "x86_64" ]; then
+        show "Downloading for x86_64 architecture..."
+        wget --quiet --show-progress "https://github.com/hemilabs/heminetwork/releases/download/$LATEST_VERSION/heminetwork_${LATEST_VERSION}_linux_amd64.tar.gz" -O "heminetwork_${LATEST_VERSION}_linux_amd64.tar.gz"
+        tar -xzf "heminetwork_${LATEST_VERSION}_linux_amd64.tar.gz" > /dev/null
+        cd "heminetwork_${LATEST_VERSION}_linux_amd64" || { show "Failed to change directory."; exit 1; }
+    elif [ "$ARCH" == "arm64" ]; then
+        show "Downloading for arm64 architecture..."
+        wget --quiet --show-progress "https://github.com/hemilabs/heminetwork/releases/download/$LATEST_VERSION/heminetwork_${LATEST_VERSION}_linux_arm64.tar.gz" -O "heminetwork_${LATEST_VERSION}_linux_arm64.tar.gz"
+        tar -xzf "heminetwork_${LATEST_VERSION}_linux_arm64.tar.gz" > /dev/null
+        cd "heminetwork_${LATEST_VERSION}_linux_arm64" || { show "Failed to change directory."; exit 1; }
+    else
+        show "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+else
+    show "Skipping download as the latest version is already present."
+fi
 
-# Удаление архива после распаковки
-rm -rf $MINER_FILE && cd heminetwork_v0.4.3_linux_amd64
+echo
+show "Select only one option:"
+show "1. Use new wallet for PoP mining"
+show "2. Use existing wallet for PoP mining"
+read -p "Enter your choice (1/2): " choice
+echo
 
-# Создание systemd сервиса для t3rn Executor
-sudo tee /etc/systemd/system/miner.service > /dev/null <<EOF
+
+if [ "$choice" == "1" ]; then
+    show "Generating a new wallet..."
+    ./keygen -secp256k1 -json -net="testnet" > ~/popm-address.json
+    if [ $? -ne 0 ]; then
+        show "Failed to generate wallet."
+        exit 1
+    fi
+    cat ~/popm-address.json
+    echo
+    read -p "Have you saved the above details? (y/N): " saved
+    echo
+    if [[ "$saved" =~ ^[Yy]$ ]]; then
+        pubkey_hash=$(jq -r '.pubkey_hash' ~/popm-address.json)
+        show "Join: https://discord.gg/hemixyz"
+        show "Request faucet from faucet channel to this address: $pubkey_hash"
+        echo
+        read -p "Have you requested faucet? (y/N): " faucet_requested
+        if [[ "$faucet_requested" =~ ^[Yy]$ ]]; then
+            priv_key=$(jq -r '.private_key' ~/popm-address.json)
+            read -p "Enter static fee (numerical only, recommended: 100-200): " static_fee
+            echo
+        fi
+    fi
+
+elif [ "$choice" == "2" ]; then
+    read -p "Enter your Private key: " priv_key
+    read -p "Enter static fee (numerical only, recommended: 100-200): " static_fee
+    echo
+fi
+
+
+if systemctl is-active --quiet hemi.service; then
+    show "hemi.service is currently running. Stopping and disabling it..."
+    sudo systemctl stop hemi.service
+    sudo systemctl disable hemi.service
+else
+    show "hemi.service is not running."
+fi
+
+cat << EOF | sudo tee /etc/systemd/system/hemi.service > /dev/null
 [Unit]
-Description=Pop Miner Service
+Description=Hemi Network popmd Service
 After=network.target
 
 [Service]
-ExecStart=/root/heminetwork_v0.4.3_linux_amd64/popmd  # Полный путь к бинарному файлу
+WorkingDirectory=$(pwd)
+ExecStart=$(pwd)/popmd
+Environment="POPM_BTC_PRIVKEY=$priv_key"
+Environment="POPM_STATIC_FEE=$static_fee"
 Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
-Environment="POPM_BTC_CHAIN_NAME=testnet3"
-Environment="POPM_BTC_PRIVKEY=$POPM_BTC_PRIVKEY"
-Environment="POPM_LOG_LEVEL=INFO"
-#Environment="POPM_PPROF_ADDRESS="
-Environment="POPM_STATIC_FEE=155"
-
-Restart=always
-RestartSec=5
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# Перезагрузка systemd для применения изменений
 sudo systemctl daemon-reload
-
-# Включение и запуск сервиса
-sudo systemctl enable miner.service
-sudo systemctl start miner.service
-
-# Показ последних 100 строк журнала и непрерывное обновление с ccze
-echo "Displaying the last 100 lines of the 'executor' service log and following updates with ccze formatting..."
-journalctl -n 100 -f -u miner | ccze -A
+sudo systemctl enable hemi.service
+sudo systemctl start hemi.service
+echo
+show "PoP mining is successfully srtated"
